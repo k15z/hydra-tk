9f57a5b9f37ce6f0eba04dbfeb20c555ec03e14d
Kevin Zhang <kevin.2015.zhang@gmail.com>
2015-06-02 22:13:13 -0700

    Cleaned _SanityCheck.cs
    MultiPerceptron and KohonenMap both work fine. In fact, KohonenMap +
    MultiPerceptron stacked together works great!

--- src/_SanityCheck.cs
+++ src/_SanityCheck.cs
@@ -1,50 +1,13 @@
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace HydraTK
 {
    class SanityCheck
    {
       public static void Main() {
-         
-      }
 
-      static void MLP() {
-         MultiPerceptron mlp = new MultiPerceptron(new int[]{2,7,1});
-         float[][] input = new float[][] {
-            new float[] {0.0f, 0.0f},
-            new float[] {0.0f, 1.0f},
-            new float[] {1.0f, 0.0f},
-            new float[] {1.0f, 1.0f}
-         };
-         float[][] output = new float[][] {
-            new float[] {0.0f},
-            new float[] {1.0f},
-            new float[] {1.0f},
-            new float[] {0.0f}
-         };
-         mlp.train(input, output);
-      }
-
-      static void KMAP() {
-         KohonenMap kmap = new KohonenMap(5, 5, 2);
-         float[][] input = new float[][] {
-            new float[] {0.0f, 0.0f},
-            new float[] {0.0f, 1.0f},
-            new float[] {1.0f, 0.0f},
-            new float[] {1.0f, 1.0f}
-         };
-         kmap.train(input);
-
-         Random rng = new Random();
-         foreach(float[] row in input) {
-            Console.WriteLine(string.Join(" ", row) + " => " + string.Join(" ", kmap.feed(row)));
-            for (int i = 0; i < 3; i++) {
-               row[0] = (float)(row[0] + rng.NextDouble()/2 - 0.5f);
-               row[1] = (float)(row[1] + rng.NextDouble()/2 - 0.5f);
-               Console.WriteLine("\t" + string.Join(" ", row) + " => " + string.Join(" ", kmap.feed(row)));
-            }
-         }
       }
    }
 }
